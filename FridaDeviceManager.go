a9ca0d6a427c2ff410468f7b4f675a6efe5ed885
fbi <97077088@qq.com>
2019-05-18 13:06:10 +0800

    update

--- FridaDeviceManager.go
+++ FridaDeviceManager.go
@@ -1,75 +1,79 @@
 package gofrida
 
-
 import "C"
 import (
 	"errors"
-	"fmt"
 )
 
-type FridaDeviceManager struct{
+type FridaDeviceManager struct {
 	ptr uintptr
 }
+
 var DefaultDeviceManager *FridaDeviceManager
-func FridaDeviceManagerFormPtr(_ptr uintptr)*FridaDeviceManager{
+
+func FridaDeviceManagerFormPtr(_ptr uintptr) *FridaDeviceManager {
 	return &FridaDeviceManager{_ptr}
 }
-func NewFridaDeviceManager()*FridaDeviceManager{
+func NewFridaDeviceManager() *FridaDeviceManager {
 	return frida_device_manager_new()
 }
-func (this* FridaDeviceManager)CPtr()uintptr{
+func (this *FridaDeviceManager) CPtr() uintptr {
 	return this.ptr
 }
 
-func (this* FridaDeviceManager)Close(){
+func (this *FridaDeviceManager) Close() {
 	frida_device_manager_close_sync(this)
 	frida_unref(this.CPtr())
 }
 
-func (this* FridaDeviceManager) GetDevice_with_id_milltimeout(_id string,_milltimeout int)(*FridaDevice,error){
+func (this *FridaDeviceManager) GetDevice_with_id_milltimeout(_id string, _milltimeout int) (*FridaDevice, error) {
 	var gerr *GError
-	d:=frida_device_manager_get_device_by_id_sync(this,_id,_milltimeout,nil,&gerr)
-	if gerr!=nil{
-		return nil,errors.New(gerr.Message())
+	d := frida_device_manager_get_device_by_id_sync(this, _id, _milltimeout, nil, &gerr)
+	if gerr != nil {
+		return nil, errors.New(gerr.Message())
 	}
-	return d,nil
+	return d, nil
 }
 
-func (this* FridaDeviceManager) EnumDevice()([]*FridaDevice,error){
+func (this *FridaDeviceManager) EnumDevice() ([]*FridaDevice, error) {
 	var gerr *GError
-	devices:=frida_device_manager_enumerate_devices_sync(this,&gerr)
-	if gerr!=nil{
-		return nil,errors.New(gerr.Message())
+	devices := frida_device_manager_enumerate_devices_sync(this, &gerr)
+	if gerr != nil {
+		return nil, errors.New(gerr.Message())
 	}
 	defer func() {
 		frida_unref(devices.CPtr())
-		devices=nil
+		devices = nil
 	}()
 	var r []*FridaDevice
-	n:=frida_device_list_size(devices)
-	for i:=0;i<n;i++{
-		d:=frida_device_list_get(devices,i)
-		d.Type=frida_device_get_dtype(d)
-		d.Name=frida_device_get_name(d)
-		d.Id=frida_device_get_id(d)
-		r=append(r,d)
+	n := frida_device_list_size(devices)
+	for i := 0; i < n; i++ {
+		d := frida_device_list_get(devices, i)
+		d.Type = frida_device_get_dtype(d)
+		d.Name = frida_device_get_name(d)
+		d.Id = frida_device_get_id(d)
+		r = append(r, d)
 	}
-	return r,nil
+	return r, nil
 }
 
-func (this* FridaDeviceManager) GetUsbDevice()(*FridaDevice,error){
-	ds,err:=this.EnumDevice()
-	if err!=nil{
-		return nil,err
+func (this *FridaDeviceManager) GetUsbDevice() (*FridaDevice, error) {
+	ds, err := this.EnumDevice()
+	if err != nil {
+		return nil, err
 	}
-	for _,d :=range ds{
-		fmt.Println(d.Type==FRIDA_DEVICE_TYPE_LOCAL)
+	var r *FridaDevice
+	for _, d := range ds {
+		if d.Type == FRIDA_DEVICE_TYPE_USB {
+			r = d
+			break
+		}
 	}
-	return nil,nil
+	return r, nil
 }
 
 //直接初始化devicemanager 好像没必要释放他
-func init(){
+func init() {
 	frida_init()
-	DefaultDeviceManager=NewFridaDeviceManager()
+	DefaultDeviceManager = NewFridaDeviceManager()
 }
